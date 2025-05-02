17d64338d5800f21df831379d3fed02a79dc557a
Aditya Agrawal <aditya.aggie@gmail.com>
2025-05-02 16:09:02 +0400

    May 02_2nd
    - facefusion\args.py
      - saving args to txt file in apply_args()
    - facefusion.ini
      - face_swapper model & pixel_boost fixed

--- facefusion/args.py
+++ facefusion/args.py
@@ -5,7 +5,7 @@ from facefusion.normalizer import normalize_fps, normalize_padding
 from facefusion.processors.core import get_processors_modules
 from facefusion.types import ApplyStateItem, Args
 from facefusion.vision import create_image_resolutions, create_video_resolutions, detect_image_resolution, detect_video_fps, detect_video_resolution, pack_resolution
-
+from datetime import datetime
 
 def reduce_step_args(args : Args) -> Args:
 	step_args =\
@@ -40,6 +40,11 @@ def collect_job_args() -> Args:
 
 
 def apply_args(args : Args, apply_state_item : ApplyStateItem) -> None:
+
+	timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	with open("args_log.txt", "a") as f:
+		f.write(f"{timestamp} - {str(args)}\n")
+
 	# general
 	apply_state_item('command', args.get('command'))
 	# paths
